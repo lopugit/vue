109f331b1478e19b59f2183cefac9ae8fdf36a16
lopugit <ireply@myleisure.com.au>
2019-12-13 20:20:11 +1100

    Added Dep.addingDep check

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -161,7 +161,10 @@ export function defineReactive (
     configurable: true,
     get: function reactiveGetter () {
       const value = getter ? getter.call(obj) : val
-      if (Dep.target) {
+      if (
+				Dep.target
+				&& !Dep.addingDep
+			) {
         dep.depend()
         if (childOb) {
           childOb.dep.depend()
